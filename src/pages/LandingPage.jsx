000b5f2de2a78913ba8ae24293cb044548bb871d
Chad96 <chadrackndalamba@gmail.com>
2024-08-15 10:24:56 +0200

    updates

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,7 +11,7 @@ const LandingPage = () => {
       <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
         <header className="mb-auto">
           <div>
-            <h3 className="float-md-start mb-0">Cover</h3>
+            <h3 className="float-md-start mb-0">Recipe Master</h3>
             <nav className="nav nav-masthead justify-content-center float-md-end">
               <Link className="nav-link active" to="/">
                 Home
